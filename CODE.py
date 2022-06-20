eafedc0ddcac00008df6dda0749c74828639e1a8
Andrey <belimoff2017@yandex.ru>
2022-06-20 10:48:18 +1000

    izmenil

--- CODE.py
+++ CODE.py
@@ -1,14 +1,7 @@
-    while True:
-        M = int(input('Message: '))
-        if M>=n:
-            print("Enter a Message less")
-        else:
-            break
-
-
-
-    F = (p - 1) * (q - 1)
-    for i in range(2, F):
-        if (gcd(i, F) == 1):
-            e = i
-            break
+elif Vopros == 1:
+    print("Enter Private Key")
+    d = int(input("d = "))
+    n = int(input("n = "))
+    c = int(input("Enter what need to decrypt  = "))  # i*e % F = 1 !@! i*d = 1 (mod F)
+    M = (c ** d) % n
+    print("Decrypt message = ",M)
