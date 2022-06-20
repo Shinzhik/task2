323a3ab3e4e1cee918791c20f51ef7b8e2884258
Leonid473 <leonid.456@inbox.ru>
2022-06-20 11:08:23 +1000

    1

---
+++ CODE.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from math import gcd
+import sympy
+
+Vopros = int(input("For Encryption Enter 0, For Decryption Enter 1 = "))
+if Vopros == 0:
+    print("Enter prime numbers")
+    while True:
+        p = int(input("p = "))
+        q = int(input("q = "))
+        if sympy.isprime(q) == False or sympy.isprime(p) == False:
+            print("Enter ONLY PRIME numbers")
+        else:
+            break
+
+    n = p * 
+
